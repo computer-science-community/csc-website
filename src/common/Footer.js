7d0a3f72c2f92ad85c1daa389701f2eb05e7722a
yxd3549 <yxd3549@rit.edu>
2020-12-30 12:16:10 -0500

    add simple footer and change header background color to match footer

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,5 +1,14 @@
+import './Footer.css';
+
 function Footer() {
-    return <h1>Footer</h1>;
+    return (<footer class="mdl-mini-footer">
+                <div class="mdl-mini-footer__left-section">
+                    <div class="mdl-logo">Copyright © Rochester Institute of Technology</div>
+                </div>
+                <div class="mdl-mini-footer__right-section">
+                    <div class="mdl-logo">One Lomb Memorial Drive Rochester NY 14623-5603</div>
+                </div>
+            </footer>);
 }
 
 export default Footer;

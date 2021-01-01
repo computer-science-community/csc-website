f4f79eed1d36de91c8ad01951155fc60b503f68e
Mayo Olojo <mayoolojo@Mayos-MBP.fios-router.home>
2020-12-31 23:22:43 -0500

    worked on the homepage, changed the tab title and logo

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,5 +1,59 @@
+import { Container, Row, Col } from "react-bootstrap";
+import './HomePage.css';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+
 function HomePage() {
-    return <h2>HomePage</h2>;
+    return (
+        <div className="home">
+            <Container>
+                <Row className="banner">
+                    <Col>
+                        <h1>Computer Science Community</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <CardDeck>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>Monthly Challenge</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer. This is a wider 
+                                        card with supporting text below as a natural lead-in to additional content. 
+                                        This content is a little bit longer.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>Upcoming Events</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer. This is a wider 
+                                        card with supporting text below as a natural lead-in to additional content. 
+                                        This content is a little bit longer.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>News</Card.Title>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This content is a little bit longer. This is a wider 
+                                        card with supporting text below as a natural lead-in to additional content. 
+                                        This content is a little bit longer.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </CardDeck>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default HomePage;

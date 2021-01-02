9ac4469f5f0cf5db2ab9937eac265f02bd0938cd
Mayo Olojo <mayoolojo@Mayos-MacBook-Pro.local>
2021-01-02 15:37:44 -0500

    pillar template

---
+++ src/pillars-temp/PillarsHome.js
@@ -0,0 +1,28 @@
+
+import { Container, Row } from 'react-bootstrap';
+import './PillarsPage.css';
+
+function PillarsHome() {
+    return(
+        <Container>
+            <Row>
+                <h1 className="pillar-h1">What is a Pillar</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio 
+                    euismod lacinia at quis risus sed vulputate. Habitasse platea 
+                    dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. 
+                    Amet volutpat consequat mauris nunc congue nisi. Tristique risus nec 
+                    feugiat in fermentum posuere urna. Aliquet lectus proin nibh nisl. 
+                    Dui accumsan sit amet nulla facilisi morbi tempus. Feugiat in ante 
+                    metus dictum at tempor commodo ullamcorper a. Faucibus in ornare quam 
+                    viverra orci sagittis eu volutpat odio. Sapien faucibus et molestie ac 
+                    feugiat sed lectus vestibulum. Ultricies integer quis auctor elit. Non 
+                    blandit massa enim nec dui nunc mattis enim. Ullamcorper a lacus vestibulum 
+                    sed arcu non odio euismod lacinia. Viverra suspendisse potenti nullam 
+                    ac tortor vitae purus.</p>
+            </Row>
+        </Container>
+    );
+}
+
+export default PillarsHome;
--- src/pillars-temp/PillarsPage.js
+++ src/pillars-temp/PillarsPage.js
@@ -1,5 +1,46 @@
+import { Container, Row, Col } from "react-bootstrap";
+import './PillarsPage.css';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import PillarsHome from './PillarsHome.js';
+import { useState } from "react";
+
 function PillarsPage() {
-    return <h2>PillarsPage</h2>;
+    const [key, setKey] = useState('home');
+    return(
+        <Container>
+            <br />
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                > {/* activeKey, onSelect handles active tab logic */}
+                {/* each Tab leads to it's respective pillar page (another js script that should be imported)
+                    disabled implies that page is not accesible from the front end (Pillars that are not pushed forward are disabled at the moment) */}
+                <Tab eventKey="home" title="Home">
+                    <PillarsHome />
+                </Tab>
+                <Tab eventKey="research" title="Research" disabled>
+                    <PillarsHome />
+                </Tab>
+                <Tab eventKey="code" title="Comptetitive Coding">
+                    <PillarsHome />
+                </Tab>
+                <Tab eventKey="mentoring" title="Mentoring">
+                    <PillarsHome />
+                </Tab>
+                <Tab eventKey="prep" title="Career Prep" disabled>
+                    <PillarsHome />
+                </Tab>
+                <Tab eventKey="review" title="Exam Review" disabled>
+                    <PillarsHome />
+                </Tab>
+                <Tab eventKey="social" title="Social Events">
+                    <PillarsHome />
+                </Tab>
+            </Tabs>
+        </Container>
+    );
 }
 
 export default PillarsPage;

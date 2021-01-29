ee0bff083a51fc5a8388492f0203f0d78c6f72f2
yxd3549 <yxd3549@rit.edu>
2021-01-28 20:54:18 -0500

    add more content to home page

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -3,8 +3,37 @@ import { Container, Row, Col } from 'react-bootstrap';
 import './HomePage.css';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
+import EventCalendar from '../common/EventCalendar';
+import Robin from '../assets/ambassadors/robin.jpg';
+import ProfileCard from '../common/ProfileCard';
 
 function HomePage() {
+  const dummyEvents = [
+    {
+      id: 1,
+      name: 'CodeSignal Workshop',
+      date: new Date(2021, 0, 30, 15, 0, 0),
+      link: 'https://campusgroups.rit.edu/',
+    },
+    {
+      id: 2,
+      name: 'Newbie Tournament',
+      date: new Date(2021, 1, 6, 20, 0, 0),
+      link: 'https://cs.rit.edu/',
+    },
+    {
+      id: 3,
+      name: 'Preping for HackerRank Skill Certification Tests',
+      date: new Date(2021, 1, 13, 20, 0, 0),
+      link: 'https://rit.edu/',
+    },
+    {
+      id: 4,
+      name: 'February Tournament',
+      date: new Date(2021, 1, 27, 20, 0, 0),
+      link: 'https://codesignal.com/',
+    },
+  ];
   return (
     <div className="home">
       <Container>
@@ -21,18 +50,28 @@ function HomePage() {
               <Card>
                 <Card.Body>
                   <Card.Title>Upcoming Events</Card.Title>
+                  <EventCalendar events={dummyEvents} />
                 </Card.Body>
               </Card>
               {/* Card for the Social Upcoming Events (Social Pillar) */}
               <Card>
                 <Card.Body>
                   <Card.Title>Featured Member</Card.Title>
+                  <ProfileCard name="Robin Avenoso" pronouns="She/Her/Hers" image={Robin} />
                 </Card.Body>
               </Card>
               {/* Card for the Golisano Wide News (Ambassadors) */}
               <Card>
                 <Card.Body>
                   <Card.Title>Featured Project</Card.Title>
+                  <p className="large-p">
+                    <strong>Critical Reminders: </strong>
+                    A Chrome Extension that finds all your
+                    assignments and quizzes on my courses and compiles it into
+                    a calendar file that can be imported into any standard
+                    calendar app.
+                  </p>
+                  <a className="large-p" href="https://github.com/SI-Squad/Zoom-a-loom">Github repo</a>
                 </Card.Body>
               </Card>
             </CardDeck>
